339f3bfbefc0b85bedf51d76c444ec74d7af3f1f
stalkcomrade <stanislavpoznyakof@gmail.com>
2019-09-03 21:46:19 +0300

    Add: pagination for stepik API and gitignore

--- stepik.py
+++ stepik.py
@@ -1,13 +1,49 @@
 import argparse
 import requests
 import json
+import os
+
+import pdb
 
 
-## TODO: account for pagination while parsing stepik grades
 
 ## don't push ids
 client_id = ""
 client_secret = ""
+
+# Generator definition for iterating over pages
+def list_pages(api_url, token):
+    has_next = True
+    page = 1
+    while has_next:
+        response = requests.get(api_url + '{}page={}'.format("&", page),
+                                headers={'Authorization': 'Bearer ' + token}).json()
+        yield response
+        page += 1
+        has_next = response['meta']['has_next']
+
+# account for pagination while parsing stepik grades
+## iterating over a generator
+## checks the folder and create if needed
+## fills in with json
+def extract_pages(api_url, token, course_id):
+
+    response = list_pages(api_url, token)
+
+    if "course_{}".format(course_id) in os.listdir("./data/raw/stepik"):
+        print("Folder already exists")
+    else:
+        # pdb.set_trace()
+        print("Creating folder ./data/raw/stepik/course_{}".format(course_id))
+        os.makedirs("./data/raw/stepik/course_{}".format(course_id))
+
+    ## if there is no next page, when don't write
+    for page in response:
+        if page['meta']['has_next'] == True:
+            with open('./data/raw/stepik/course_{}/course_grades_{}_page_{}.json'.format(course_id,course_id, page['meta']['page']), 'w') as outfile:
+                json.dump(page, outfile)
+        else:
+            print("Last page is parsed (page {})".format(page['meta']['page']))
 
 ## wrap into function
 def fetch_course_data(client_id, client_secret, course_id):
@@ -29,10 +65,10 @@ def fetch_course_data(client_id, client_secret, course_id):
 
     course_grades = requests.get(api_url,
                         headers={'Authorization': 'Bearer ' + token}).json()
+    
+    extract_pages(api_url, token, course_id)
 
-    # print(course_grades)
-
-    ## FIXME: write to file OR stream directly to R
+    ## write to file OR stream directly to R
     with open('course_grades_{}.json'.format(course_id), 'w') as outfile:
         json.dump(course_grades, outfile)
 
