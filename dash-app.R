1db6fe6c6637147e4e80f869c2919c07538f4858
stalkcomrade <stanislavpoznyakof@gmail.com>
2019-09-03 17:48:55 +0300

    Add: simple bar chart

--- dash-app.R
+++ dash-app.R
@@ -2,9 +2,17 @@ library(dash)
 library(dashHtmlComponents)
 library(dashCoreComponents)
 library(dashTable)
+
 library(stringr)
+library(tidyverse)
 
 app <- Dash$new()
+
+## TODO: as input
+stepik_ranks = read_csv("./course_df_3122.csv")
+
+
+## TODO: make a structure for a project
 
 ## import 
 ## import::here(app2, .from = "./apps/app-2.R")
@@ -31,10 +39,25 @@ page_1_layout = htmlDiv(children = list(
 ))
 
 
+
 page_2_layout = htmlDiv(children = list(
   htmlH3("App 2"),
   dccInput(id = "inputID", value = "initial value", type = "text"),
-  htmlDiv(id = "outputID")
+  htmlDiv(id = "outputID"),
+  ## should be carefull with nested lists
+  dccGraph(id = "giraffe",
+           figure = list(
+             data = list(list(x = c(1,2,3), y = c(3,2,8), type = 'bar')),
+             layout = list(title = "Let's Dance!")
+           )
+           ),
+  dccGraph(id = "giraffe-2",
+           figure = list(
+             data = list(list(x = as.character(stepik_ranks$user), y = stepik_ranks$rank, type = 'bar')),
+             layout = list(title = "Students' Ranks", xaxis = list(type = "category",
+                                                                   title = list(text = "x axis")))
+           )
+           )
 ))
 
 ## page 2 calback
@@ -58,7 +81,7 @@ app$callback(output = list(id = "page-content", property = "children"),
                  return(index_page)
                }
              }
-)
+             )
 
-        
-app$run_server(showcase = TRUE, debug = TRUE, dev_tools_hot_reload = TRUE)
+
+app$run_server(showcase = TRUE, debug = TRUE)
