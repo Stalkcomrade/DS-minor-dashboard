d67d669701d92bb343bbb73aad7b4930e1d5f377
stalkcomrade <stanislavpoznyakof@gmail.com>
2019-09-04 13:23:03 +0300

    ADDED: parsing json with type-checking

--- parsing_course_grades.R
+++ parsing_course_grades.R
@@ -1,48 +1,68 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
 
-
 library(tidyverse)
 library(jsonlite)
 
-## TODO: prepare function
+## change paths
+## prepare function
+
 prepare_students_reports = function(course_id) {
 
-  vf = jsonlite::fromJSON(str_c("course_grades_", course_id, ".json"),
+  ## create a folder if doesn't exist
+  if (sprintf("course_%s", course_id) %in% 
+      str_replace(pattern = "./data/processed/stepik/", replacement = "", list.dirs("./data/processed/stepik"))) {
+    print(sprintf("Folder for processed data (csv) per %s already exists", course_id))
+  } else {
+    print(sprintf("Creating a folder for processed data (csv) per %s", course_id))
+    dir.create(sprintf("./data/processed/stepik/course_%s", course_id))
+  }
+
+
+## FIXME: replace Nulls with zeros (for now)
+rewrite_nulls = function(x) {
+  if (is.null(x)) {
+    return(0)
+  } else {
+    return(x)
+  }
+}
+
+## iterates over all json files 
+
+i = 0
+for (page in list.files(sprintf("./data/raw/stepik/course_%s/", course_id))) {
+
+  i = i + 1
+  df = NULL
+  ## pagination
+  vf = jsonlite::fromJSON(sprintf("./data/raw/stepik/course_%s/course_grades_%s_page_%s.json", course_id, course_id, i),
                           simplifyVector = FALSE)
 
-  vf[[2]] # object of arrays
-  vf[[2]] %>% class()
 
   df = tibble(user = numeric(), score = numeric(), course = numeric(), rank = numeric())
 
-  vf[[2]] %>% map(.f = function(.x) {
+  tryCatch({
+
+vf[[2]] %>% map(.f = function(.x) {
 
     ## not the best practice
     df <<- add_row(.data = df,
                    user = .x$user,
                    score = .x$score,
                    course = .x$course,
-                   rank = .x$rank)
+                   rank = rewrite_nulls(.x$rank))
 
   })
+  write_csv(df, sprintf("./data/processed/stepik/course_%s/course_df_%s_page_%s.csv", course_id, course_id, i))
 
-  write_csv(df, str_c("course_df_", course_id, ".csv"))
+  }, error = function(cond) {
+
+    print(sprintf("Error %s", cond))
+    print(sprintf("Error %s in course, %s page ", course_id, i))
+
+  })
+}
 }
 
 prepare_students_reports(args[1])
-
-
-# vf = jsonlite::fromJSON("course_grades.json", simplifyVector = FALSE)
-# vf[[2]] # object of arrays
-# vf[[2]] %>% class()
-# df = tibble(user = numeric(), score = numeric(), course = numeric(), rank = numeric())
-# vf[[2]] %>% map(.f = function(.x) {
-#   ## not the best practice
-#    df <<- add_row(.data = df,
-#           user = .x$user,
-#           score = .x$score,
-#           course = .x$course,
-#           rank = .x$rank)
-# })
-
