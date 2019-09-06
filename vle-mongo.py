9e0c514dcffee9360937f677f39742deba72bda9
stalkcomrade <stanislavpoznyakof@gmail.com>
2019-09-06 13:48:41 +0300

    UPD: config files

--- vle-mongo.py
+++ vle-mongo.py
@@ -1,4 +1,53 @@
 # /srv/store/principal/audit/r-console/
-
-# mongo —username vle —password vfmke45gv9bg vle 
+# mongo --host mongo.piterdata.ninja --username vle --password vfmke45gv9bg vle 
 # db.logs.find({'entry_type': 'gist'})
+
+import argparse
+from pymongo import MongoClient
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Fetches MongoDB with logs')
+    parser.add_argument('-u',   '--user',            help='username',    required=True)
+    parser.add_argument('-pwd', '--password',        help='password',    required=True)
+    parser.add_argument('-auth','--authSource',      help='auth source', required=True)
+    args = vars(parser.parse_args())
+    return args
+    
+username = parse_args()['user']
+password = parse_args()['password']
+authSource = parse_args()['authSource']
+
+client = MongoClient('mongodb://%s:%s@mongo.piterdata.ninja' % (username, password), authSource=authSource)
+
+db = client.vle
+users = db.users.find()
+
+## using lists as array
+## dicts are objects
+tempList = list()
+
+for user in users:
+    tempList.append(user)
+
+print(client)
+print(db)
+print(users)
+print(tempList)
+
+#### users db
+
+## TODO: display: 
+#   group
+#   programme
+#   stepik_id (for matching)
+#   filter by a server
+
+
+#### 
+
+
+
+## TODO: read individual jsonl files from a remote server
+
+# if __name__== "__main__":
+#     main()
